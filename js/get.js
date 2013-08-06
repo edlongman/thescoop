ecb405aab396fcec91f3a66deced6c88a1c817f8
Samuel Hönle <samuel.hoenle@gmail.com>
2013-08-06 15:47:31 +0100

    Sorry if this fucks up anything
    I was working on that during the restructure, gonna fix it later.
    Fixed some things, inserted test values, and it's actually working now.
    
    - amount and scope have test values now
    - fixed switch
    - made ajax call async to be able to return the result

--- js/get.js
+++ js/get.js
@@ -6,45 +6,49 @@
 today = new Date(); // dates are entered relatively, today is needed
 start_time = new Date();
 end_time = today;
-categories;
+categories = '';
 
 function getJSON(){
-  // get amount of days/months user has been away
-  // amount = $('#idOfInput').val();
+    // get amount of days/months user has been away
+    // amount = $('#idOfInput').val();
+    amount = 1;
 
-  // get scope (days/months)
-  // amount = $('#idOfSelect option:selected').val();
+    // get scope (days/months)
+    // scope = $('#idOfSelect option:selected').val();
+    scope = 'days';
 
-  setStartTime(amount, scope);
+    setStartTime(amount, scope);
 
-  // get categories
-  // categories = $('#whatever').whatever();
+    // get categories
+    // categories = $('#whatever').whatever();
+    categories = '';
 
-  json = ajax();
-
-  // doSomething with JSON, or not
-
-  return json;
+    json = ajax();
+    // place for further processing
+    return json;
 }
 
 function setStartTime(amount, scope){
-  switch scope {
-    case 'days': start_time.setDate(today.getDate()-amount); break;
-    case 'months': start_time.setMonth(today.getMonth()-amount); break;
-  }
+    switch (scope) {
+        case 'days': start_time.setDate(today.getDate()-amount); break;
+        case 'months': start_time.setMonth(today.getMonth()-amount); break;
+    }
 }
 
 function ajax(){
+    data = [];
     $.ajax({
-      url: '/path/to/file',
-      type: 'GET',
-      dataType: 'json',
-      data: {start_time: start_time.toJSON().substring(0,10), end_time: end_time.toJSON().substring(0,10), categories: categories},
-      success: function(data, textStatus, xhr) {
-        return data;
-      },
-      error: function(xhr, textStatus, errorThrown) {
-        log('ERROR: ' + errorThrown + '; ' + xhr);
-      }
+        url: '../include.php',
+        type: 'GET',
+        dataType: 'json',
+        data: {start_time: start_time.toJSON().substring(0,10), end_time: end_time.toJSON().substring(0,10), categories: categories},
+        async: false,
+        success: function(response, textStatus, xhr) {
+            data = response;
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            console.log('ERROR: ' + errorThrown + '; ' + xhr);
+        }
     });
+    return data;
 }
