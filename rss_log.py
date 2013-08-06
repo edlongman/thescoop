58d5ec1002f1e16f836dec965feb6ae37a84492e
Taiiwo <tylerzacc@gmail.com>
2013-08-06 21:39:16 +0100

    Update rss_log.py

--- rss_log.py
+++ rss_log.py
@@ -9,10 +9,10 @@ items = feed [ 'items' ]
 i = 0
 while i < len(items):
     if sql.query('SELECT * FROM bbcfeeds WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':
-      sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + (len(items) - i) * 4)
+      sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + (len(items) - i * 1.002))
     else:
         curpoints = sql.query('SELECT FROM points WHERE url = ' + items[i]['link'])
-        sql.query('UPDATE url = ' + items[i]['link'] + ' INSERT points ' + intcurpoints + curpoints * 1.002)
+        sql.query('UPDATE url = ' + items[i]['link'] + ' INSERT points ' + str(int(curpoints + curpoints * 1.002)))
     i = i + 1
 
 
