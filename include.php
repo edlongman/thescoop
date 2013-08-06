32ca88122c159cfb614a18055521c4c1485e4e82
Taiiwo <tylerzacc@gmail.com>
2013-08-06 12:51:57 +0100

    Update include.php
    Added get_news().

--- include.php
+++ include.php
@@ -1,3 +1,11 @@
 <?php
-
+  include_once util.php;
+  function get_news($start_time, $end_time) { //returns a multidimentional array of titles and URLs of news between time frames
+     $json = get_url_output('http://content.guardianapis.com/search?from-date='. $start_time . '&to-date=' . $end_time . '&order-by=relevance&format=json');
+     $json_obj = JSON_decode($json);
+     $titles_array = $json_obj->response->results->webTitle;
+     $url_array = $json_obj->response->results->webUrl;
+     return($titles_array, $url_array);
+  }
+  echo get_news($_REQUEST['start_time'],$_REQUEST['end_time'])[0,1];
 ?>
