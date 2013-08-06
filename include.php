0cff457bb4ffd04eda9b83c742bfdace42fa36a9
Ed Longman <e.i.a.longman@gmail,com>
2013-08-06 14:03:24 +0100

    completed initial json api

--- include.php
+++ include.php
@@ -1,12 +1,18 @@
 <?php
   include_once 'util.php';
   function get_news($start_time, $end_time) { //returns a multidimentional array of titles and URLs of news between time frames
+     header("Content-type: text/json");
      $json = get_url_output('http://content.guardianapis.com/search?from-date='. $start_time . '&to-date=' . $end_time . '&order-by=relevance&format=json');
      $json_obj = JSON_decode($json,true);
-     $titles_array = $json_obj->response->results->webTitle;
-     $url_array = $json_obj->response->results->webUrl;
+	 //print_r($json_obj["response"]["results"][0]["webTitle"]);
+	 $titles_array = array();
+	 $url_array= array();
+	 for($i=0;$i<count($json_obj["response"]["results"]);$i++){
+     	$titles_array[] = $json_obj["response"]["results"][$i]["webTitle"];
+     	$url_array[] = $json_obj["response"]["results"][$i]["webUrl"];
+	 }
      $ret_me = array($titles_array , $url_array);
      return($ret_me);
   }
-  print_r(get_news($_REQUEST['start_time'],$_REQUEST['end_time']));
+  echo json_encode(get_news($_REQUEST['start_time'],$_REQUEST['end_time']));
 ?>
