bc487fee002ade20a6c116c8e6134f2b28ae286b
Taiiwo <tylerzacc@gmail.com>
2013-08-06 22:33:18 +0100

    Update rss_log.py

--- rss_log.py
+++ rss_log.py
@@ -9,7 +9,7 @@ items = feed [ 'items' ]
 i = 0
 while i < len(items):
     if sql.query('SELECT id FROM bbcfeeds WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
-      sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
+        sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
     else:
         curpoints = sql.query('SELECT FROM points WHERE url = ' + items[i]['link'])#else get the current point value
         sql.query('UPDATE bbcfeeds  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
