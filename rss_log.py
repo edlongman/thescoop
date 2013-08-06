2614b7c047be7de960f4e1bb890b46500254c29e
Taiiwo <tylerzacc@gmail.com>
2013-08-06 22:08:12 +0100

    Update rss_log.py
    Fixed a bit more MySQL

--- rss_log.py
+++ rss_log.py
@@ -8,14 +8,12 @@ feed = feedparser.parse(url)
 items = feed [ 'items' ]
 i = 0
 while i < len(items):
-    if sql.query('SELECT * FROM bbcfeeds WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':
-      sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))
+    if sql.query('SELECT id FROM bbcfeeds WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
+      sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
     else:
-        curpoints = sql.query('SELECT FROM points WHERE url = ' + items[i]['link'])
-        sql.query('UPDATE url = ' + items[i]['link'] + ' INSERT points ' + curpoints * ( 1.02 / (i + 1))
-        
-        curpoints * ( 1.02 / (i + 1))
+        curpoints = sql.query('SELECT FROM points WHERE url = ' + items[i]['link'])#else get the current point value
+        sql.query('UPDATE bbcfeeds  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
     i = i + 1
-
+#Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
 
 
