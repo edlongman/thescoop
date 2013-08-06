cbe0d08b869a532fdaf01bb9d8c3de22863c3bef
Ed Longman <e.i.a.longman@gmail,com>
2013-08-06 15:20:41 +0100

    restructured into site. lib

---
+++ site/js/get.js
@@ -0,0 +1,50 @@
+/*****************************************************************************/
+/*                        Get JSON response from PHP                         */
+/*****************************************************************************/
+
+//defining global attributes
+today = new Date(); // dates are entered relatively, today is needed
+start_time = new Date();
+end_time = today;
+categories;
+
+function getJSON(){
+  // get amount of days/months user has been away
+  // amount = $('#idOfInput').val();
+
+  // get scope (days/months)
+  // amount = $('#idOfSelect option:selected').val();
+
+  setStartTime(amount, scope);
+
+  // get categories
+  // categories = $('#whatever').whatever();
+
+  json = ajax();
+
+  // doSomething with JSON, or not
+
+  return json;
+}
+
+function setStartTime(amount, scope){
+  switch scope {
+    case 'days': start_time.setDate(today.getDate()-amount); break;
+    case 'months': start_time.setMonth(today.getMonth()-amount); break;
+  }
+}
+
+function ajax(){
+    $.ajax({
+      url: '/path/to/file',
+      type: 'GET',
+      dataType: 'json',
+      data: {start_time: start_time.toJSON().substring(0,10), end_time: end_time.toJSON().substring(0,10), categories: categories},
+      success: function(data, textStatus, xhr) {
+        return data;
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        log('ERROR: ' + errorThrown + '; ' + xhr);
+      }
+    });
+}
