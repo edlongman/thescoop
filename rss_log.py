4517409987e44d306d5ecac44bc66e5cce0ebf99
Ed Longman <e.i.a.longman@gmail,com>
2013-08-07 14:30:17 +0100

    changed how it asks the database

--- rss_log.py
+++ rss_log.py
@@ -11,10 +11,13 @@ i = 0
 while i < len(items):
     item=items[i];
 	conditions=' WHERE url="' + item['guid'] + '" AND  date_added=CURDATE()'
-	if sql.query('SELECT points FROM '+ table_name + conditions) == '':#if the url has not been logged today
+	sql.query('SELECT points FROM '+ table_name + conditions)
+	points_result=sql.store_result();
+	if points_result.num_rows()==0:#if the url has not been logged today
         sql.query('INSERT INTO '+ table_name +' (`url`,`title`,`description`,`date`)("' + item['guid'] + '","' + item['title'] + '"' + item['summary'] + '",' + 1 * (1.02 / (i + 1)) + ',CURDATE())')#then add it to the db, setting points to 1 * (1.02/list_postition)
+		insert_result=sql.store_result();
     else:
-        curpoints = sql.query('SELECT `points` FROM '+ table_name + conditions)#else get the current point value
+		currpoints=points_result.fetch_row()[0][0]
         sql.query('UPDATE '+ table_name +' SET points=' + curpoints * ( 1.02 / (i + 1)) + conditions)#and update the points
     i = i + 1
 #Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
