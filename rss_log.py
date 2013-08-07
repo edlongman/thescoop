aee809f7911d00ab9a8d98027d1ab527fe6de34f
Ed Longman <eialongman@gmail.com>
2013-08-07 18:05:28 -0400

    added output file frpm python

--- rss_log.py
+++ rss_log.py
@@ -1,4 +1,4 @@
-import MySQLdb, feedparser, config, urllib, cgi
+import MySQLdb, feedparser, config, urllib, cgi, datetime
 sql = MySQLdb.connect(host="localhost", 
                             user=config.username, 
                             passwd=config.passwd,
@@ -22,10 +22,11 @@ while i < len(items):
 		insert_result=sql.store_result()
 	else:
 		row = points_result.fetch_row()
-		print row
 		currpoints=float(row[0][0])
 		sql.query('UPDATE '+ table_name +' SET points=' + str(algo(currpoints,i)) + conditions)#and update the points
 	i = i + 1
+timenow=datetime.datetime.now()
+print "success at: " + timenow.strftime('%d-%m-%y %H:%M\n')
 #Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
 
 
