5d4f3d5b0c074710d0c36a6f130954534bb4acee
bleko <bleko@server.fake>
2013-08-07 18:08:54 +0100

    Input and select elements resize on window resize

--- site/js/whileaway.js
+++ site/js/whileaway.js
@@ -1,31 +1,37 @@
 // Form UI fixes
 
 $('#keyword').change(function() {
-    option_val = $('#keyword > option:selected').val();
-    $('#keyword-span').html(option_val);
-    $('#keyword').width($('#keyword-span').width());
+	option_val = $('#keyword > option:selected').val();
+	$('#keyword-span').html(option_val);
+	$('#keyword').width($('#keyword-span').width());
 });
 
 $('#keyword').change();
 
 
 $('#date').change(function() {
-    option_val = $('#date > option:selected').val();
-    $('#date-span').html(option_val);
-    $('#date').width($('#date-span').width());
+	option_val = $('#date > option:selected').val();
+	$('#date-span').html(option_val);
+	$('#date').width($('#date-span').width());
 });
 
 $('#date').change();
 
 
 function inputWidth() {
-    $('#number-span').html($('#number').val());
-    $('#number').css('width', $('#number-span').width());
+	$('#number-span').html($('#number').val());
+	$('#number').css('width', $('#number-span').width());
 }
 
 inputWidth();
 
 $('#number').bind('keyup input paste', inputWidth);
+
+window.onresize = function() {
+	$('#keyword').change();
+	$('#date').change();
+	inputWidth();
+}
 
 
 
@@ -40,50 +46,50 @@ $('#date').change(function(){getNews();});
 $('#keyword').change(function(){getNews();});
 
 function getNews(){
-    amount = $('#number').val();
-    scope = $('#date option:selected').val();
-    keyword = $('#keyword option:selected').val();
-    if (keyword == 'news') {
-        keyword = '';
-    };
+	amount = $('#number').val();
+	scope = $('#date option:selected').val();
+	keyword = $('#keyword option:selected').val();
+	if (keyword == 'news') {
+		keyword = '';
+	};
 
-    try {
-        validate(amount, scope, keyword);
-    } catch (e) {
-        alert(e);
-        return;
-    }
+	try {
+		validate(amount, scope, keyword);
+	} catch (e) {
+		alert(e);
+		return;
+	}
 
-    // make asynchronous request
-    getNewsFromAPI(amount, scope, keyword);
+	// make asynchronous request
+	getNewsFromAPI(amount, scope, keyword);
 }
 
 function handleNews(news){
-    $('#form button').html('Go');
+	$('#form button').html('Go');
 
-    str = '<ol>';
-    $.each(news, function(index, story) {
-         headline = story[0];
-         link = story[1];
-         date = story[2];
-         // summary = story[3];
+	str = '<ol>';
+	$.each(news, function(index, story) {
+		headline = story[0];
+		link = story[1];
+		date = story[2];
+		// summary = story[3];
 
-         // str += '<li>' + headline + ' (<a href="' + link + '">more…</a>, ' + date.f('d MMM yyyy HH:mm') + ')</li>';
-         str += '<li><a href="' + link + '">' + headline + '</a></li>';
-    });
-    str += '</ol>';
-    $('#headlines').html(str);
+		// str += '<li>' + headline + ' (<a href="' + link + '">more…</a>, ' + date.f('d MMM yyyy HH:mm') + ')</li>';
+		str += '<li><a href="' + link + '">' + headline + '</a></li>';
+	});
+	str += '</ol>';
+	$('#headlines').html(str);
 }
 
 function validate(amount, scope, keyword) {
-    scopes = ['days', 'weeks', 'months'];
-    if (scopes.indexOf(scope) < 0) {
-        throw 'Invalid scope';
-    }
+	scopes = ['days', 'weeks', 'months'];
+	if (scopes.indexOf(scope) < 0) {
+		throw 'Invalid scope';
+	}
 
-    if (isNaN(amount)) {
-        throw 'Invalid amount';
-    }
+	if (isNaN(amount)) {
+		throw 'Invalid amount';
+	}
 
-    $.trim(keyword);
+	$.trim(keyword);
 }
