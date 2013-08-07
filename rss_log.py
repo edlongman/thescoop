883dcb6f90379f127cd84a38213fc1f3f81e1686
Ed Longman <eialongman@gmail.com>
2013-08-07 13:47:51 -0400

    added full functionality : it works

--- rss_log.py
+++ rss_log.py
@@ -1,27 +1,28 @@
-import MySQLdb, feedparser, config
-sql = MySQLdb.connect(host="localhost", # your host, usually localhost
-                            user=config.username, # your username
-                            passwd=config.pass, # your password
-                            db=config.db) # name of the data base
+import MySQLdb, feedparser, config, re
+sql = MySQLdb.connect(host="localhost", 
+                            user=config.username, 
+                            passwd=config.passwd,
+                            db=config.db)
 rss_url = "http://feeds.bbci.co.uk/news/rss.xml?edition=uk"
 table_name = "bbcstories"
 feed = feedparser.parse(rss_url)
 items = feed [ 'items' ]
 i = 0
 def algo(score,pos):
-	return num * (1.02 / (pos + 1))
+	return score * (1.02 / (pos + 1))
 while i < len(items):
 	item=items[i];
 	conditions=' WHERE url="' + item['guid'] + '" AND  date_added=CURDATE()'
 	sql.query('SELECT points FROM '+ table_name + conditions)
 	points_result=sql.store_result();
 	if points_result.num_rows()==0:#if the url has not been logged today
-		sql.query('INSERT INTO '+ table_name +' (`url`,`title`,`description`,`points`,`date`)
-		VALUES("' + item['guid'] + '","' + item['title'] + '"' + item['summary'] + '",' + algo(1,i) + ',CURDATE())')#then add it to the db, setting points to 1 * (1.02/list_postition)
+		item['title']=re.escape(item['title'])
+		item['summary']=re.escape(item['summary'])
+		sql.query('INSERT INTO '+ table_name +' (`url`,`title`,`description`,`points`,`date_added`) VALUES("' + item['guid'] + '","' + item['title'] + '","' + item['summary'] + '",' + str(algo(1,i)) + ',CURDATE())')#then add it to the db, setting points to 1 * (1.02/list_postition)
 		insert_result=sql.store_result();
 	else:
 		currpoints=float(points_result.fetch_row()[0][0])
-		sql.query('UPDATE '+ table_name +' SET points=' + algo(currpoints,i) + conditions)#and update the points
+		sql.query('UPDATE '+ table_name +' SET points=' + str(algo(currpoints,i)) + conditions)#and update the points
 	i = i + 1
 #Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
 
