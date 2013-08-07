f6378285b0c1f4744b38e886ad910655dd8eaa35
Taiiwo Llort <tylerzacc@gmail.com>
2013-08-07 10:02:28 +0100

    new file:   feedparser.pyc 	modified:   rss_log.py 	modified:   site/test.html

--- rss_log.py
+++ rss_log.py
@@ -1,4 +1,4 @@
-import MySQLdb, feedparser
+ import MySQLdb, feedparser
 sql = MySQLdb.connect(host="localhost", # your host, usually localhost
                             user="root", # your username
                             passwd="", # your password
@@ -8,11 +8,11 @@ feed = feedparser.parse(url)
 items = feed [ 'items' ]
 i = 0
 while i < len(items):
-    if sql.query('SELECT id FROM bbcfeeds WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
-        sql.query('INSERT INTO news_log ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
+    if sql.query('SELECT id FROM bbcstories WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
+        sql.query('INSERT INTO bbcstories ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
     else:
-        curpoints = sql.query('SELECT FROM points WHERE url = ' + items[i]['link'])#else get the current point value
-        sql.query('UPDATE bbcfeeds  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
+        curpoints = sql.query('SELECT points FROM bbcstories WHERE url = ' + items[i]['link'])#else get the current point value
+        sql.query('UPDATE bbcstories  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
     i = i + 1
 #Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
 
