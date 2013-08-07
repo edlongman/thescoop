e6b401cec44498bbd49bb206389f6b7620f1222e
Samuel Hönle <samuel.hoenle@gmail.com>
2013-08-07 20:13:19 +0100

    Place for summary created, show/hide on click
    Styling will be discussed tomorrow // is left to Jake

--- site/js/whileaway.js
+++ site/js/whileaway.js
@@ -2,52 +2,50 @@
 /*                          Main JS for whileaway                            */
 /*****************************************************************************/
 
-// Form UI fixes
+$(document).ready(function(){
+    // Form UI fixes
 
-$('#keyword').change(function() {
-	option_val = $('#keyword > option:selected').val();
-	$('#keyword-span').html(option_val);
-	$('#keyword').width($('#keyword-span').width());
+    $('#keyword').change(function() {
+    	option_val = $('#keyword > option:selected').val();
+    	$('#keyword-span').html(option_val);
+    	$('#keyword').width($('#keyword-span').width());
+    });
+
+    $('#keyword').change();
+
+
+    $('#date').change(function() {
+    	option_val = $('#date > option:selected').val();
+    	$('#date-span').html(option_val);
+    	$('#date').width($('#date-span').width());
+    });
+
+    $('#date').change();
+
+
+    function inputWidth() {
+    	$('#number-span').html($('#number').val());
+    	$('#number').css('width', $('#number-span').width());
+    }
+
+    inputWidth();
+
+    $('#number').bind('keyup input paste', inputWidth);
+
+    window.onresize = function() {
+    	$('#keyword').change();
+    	$('#date').change();
+    	inputWidth();
+    }
+
+    // Fetching and displaying stories
+    getNews();
+
+    // onchange of input fields, call getNews()
+    $('#number').bind('keyup input paste', function(){getNews();});
+    $('#date').change(function(){getNews();});
+    $('#keyword').change(function(){getNews();});
 });
-
-$('#keyword').change();
-
-
-$('#date').change(function() {
-	option_val = $('#date > option:selected').val();
-	$('#date-span').html(option_val);
-	$('#date').width($('#date-span').width());
-});
-
-$('#date').change();
-
-
-function inputWidth() {
-	$('#number-span').html($('#number').val());
-	$('#number').css('width', $('#number-span').width());
-}
-
-inputWidth();
-
-$('#number').bind('keyup input paste', inputWidth);
-
-window.onresize = function() {
-	$('#keyword').change();
-	$('#date').change();
-	inputWidth();
-}
-
-
-
-
-// Fetching and displaying stories
-
-getNews();
-
-// onchange of input fields, call getNews()
-$('#number').bind('keyup input paste', function(){getNews();});
-$('#date').change(function(){getNews();});
-$('#keyword').change(function(){getNews();});
 
 function getNews(){
 	amount = $('#number').val();
@@ -79,10 +77,18 @@ function handleNews(news){
 		// summary = story[3];
 
 		// str += '<li>' + headline + ' (<a href="' + link + '">more…</a>, ' + date.f('d MMM yyyy HH:mm') + ')</li>';
-		str += '<li><a href="' + link + '">' + headline + '</a></li>';
+        str += '<li class="headline">';
+		str += '<a href="#">' + headline + '</a>';
+        str += '<div><p>Insert summary here</p><span class="read-more-link"><a href="' + link + '">Read more…</a></span></div>'
+        str += '</li>  '
 	});
 	str += '</ol>';
 	$('#headlines').html(str);
+
+    // display summary on headline click
+    $('.headline a').click(function(e){
+        $(this).next('div').toggle(300);
+    });
 }
 
 function validate(amount, scope, keyword) {
