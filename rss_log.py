3eb347e1134a70638a71effa28090af45a14c39b
Taiiwo Llort <tylerzacc@gmail.com>
2013-08-07 10:45:14 +0100

    modified:   rss_log.py

--- rss_log.py
+++ rss_log.py
@@ -3,16 +3,17 @@ sql = MySQLdb.connect(host="localhost", # your host, usually localhost
                             user="root", # your username
                             passwd="", # your password
                             db="agnes") # name of the data base
-rss_url = ""
-feed = feedparser.parse(url)
-items = feed [ 'items' ]
+rss_url = "http://feeds.bbci.co.uk/news/rss.xml"
+mysql_table = "bbcstories"
+feed = feedparser.parse(rss_url)
+items = feed[ 'items' ]
 i = 0
 while i < len(items):
-    if sql.query('SELECT id FROM bbcstories WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
-        sql.query('INSERT INTO bbcstories ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
+    if sql.query('SELECT id FROM '+ mysql_table +' WHERE url=' + items[i]['link'] + ' AND  date=' + items[i]['published'] ) == '':#if the url has not been logged today
+        sql.query('INSERT INTO '+ mysql_table +' ' + items[i]['link'] + ',' + items[i] + ',' + items[i]['summary'] + ',' + 1 * (1.02 / (i + 1))#then add it to the db, setting points to 1 * (1.02/list_postition)
     else:
-        curpoints = sql.query('SELECT points FROM bbcstories WHERE url = ' + items[i]['link'])#else get the current point value
-        sql.query('UPDATE bbcstories  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
+        curpoints = sql.query('SELECT points FROM '+ mysql_table +' WHERE url = ' + items[i]['link'])#else get the current point value
+        sql.query('UPDATE '+ mysql_table +'  SET points=' + curpoints * ( 1.02 / (i + 1)) + 'WHERE url=' + items[i]['link'] + 'AND date=' + items[i]['published'])#and update the points
     i = i + 1
 #Fields I want: id INT, url VARSTRING(255), date (#a mysql date format), title VARSTRING(140), desc VARSTRING(255), points INT
 
