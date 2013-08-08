71d5b283e1db54cd4b3ba9d4f239e902d16907e0
Taiiwo Llort <tylerzacc@gmail.com>
2013-08-08 10:53:20 +0100

    updated include.php

--- site/include.php
+++ site/include.php
@@ -19,7 +19,7 @@
 		}
 		return($ret_me);
 	}
-	function daily_news2($start_time, $end_time) {
+	function daily_news2($start_time, $end_time) {//This function doesn't work. Don't use it.
 		//echo strtotime($end_time) - strtotime($start_time) / 86400;
 		$num_years = implode( '', array_slice( str_split( $end_time ), 0, 4 ) ) + 0 - implode( '', array_slice( str_split( $end_time ), 0, 4 ) ) + 0;
 		for($i=0; $i<=$num_years; $i++) {
@@ -39,15 +39,15 @@
 		}
 		return($daily_news);
 	}
-	function daily_news($start_time, $end_time) {
-		$start_date = strtotime($start_time);
-		$end_date = strtotime($end_time);
-		$num = ($end_date - $start_date) / (60*60*24);
+	function daily_news($start_time, $end_time) { //Returns news for each day between start_time and end_time. Caps at 200 days of news.
+		$start_date = strtotime($start_time);//changes start time to seconds
+		$end_date = strtotime($end_time);//^*end time*^
+		$num = ($end_date - $start_date) / (60*60*24);//calculates number of days between dates
 		//echo $num;
-		if($num <= 200) {
-			for($i=0;$i <= $num;$i++) {
+		if($num <= 200) {//level cap to stop overflow
+			for($i=0;$i <= $num;$i++) { //for each day between the dates, fetch news and add it to $daily_news in array form.
 				$daily_news[] = get_news(date('Y-m-d',$start_date + ($i*60*60*24)),date('Y-m-d',$start_date + ($i*60*60*24)),$_REQUEST['section'],$_REQUEST['keyword']);
-				//$daily_news[] = date('Y-m-d',$start_date + ($i*60*60*24));
+				//$daily_news[] = date('Y-m-d',$start_date + ($i*60*60*24)); //Used to check dates were increasing properly in the correct format
 			}
 		}
 		else {
@@ -55,7 +55,7 @@
 		}
 		return($daily_news);
 	}
-	function two_char_format($number) { //if $number is 1 char long, return it with a zero infront, else do nothing.
+	function two_char_format($number) { //if $number is 1 char long, return it with a zero infront, else return $number.
 		if(count(str_split($number))==1) {
 			return('0' . $number);
 		}
