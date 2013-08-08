7723d1c2879916ad55b150365e83c2eca24de9c4
Samuel Hönle <samuel.hoenle@gmail.com>
2013-08-08 14:05:11 +0100

    HOTFIX: use right API

--- site/js/ajax.js
+++ site/js/ajax.js
@@ -22,18 +22,18 @@ function getGuardianNews(amount, scope, section, keyword){
 
 function ajaxGuardian(start_time, end_time, section, keyword){
     $.ajax({
-        url: 'include.php',
+        url: 'guardian_feeds.php',
         type: 'GET',
         dataType: 'json',
         data: {start_time: start_time.f('yyyy-MM-dd'), end_time: end_time.f('yyyy-MM-dd'), section: section, keyword: keyword},
         success: function(data, textStatus, xhr) {
             // replace JSON date format with JavaScript Date Objects
-            $.each(data[0], function(index, story) {
+            $.each(data, function(index, story) {
                  story[2] = new Date(story[2]);
             });
 
             // call handleGuardianNews function of whileaway.js
-            handleGuardianNews(data[0]);
+            handleGuardianNews(data);
         },
         error: function(xhr, textStatus, errorThrown) {
             console.log('ERROR: ' + errorThrown);
