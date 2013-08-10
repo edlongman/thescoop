86d636cb406e23340baaa38f6043df9a5d0d15a9
edlongman <e.i.a.longman@gmail.com>
2013-08-10 19:28:04 +0100

    Cleanup

--- Run_This_Very_Important.py
+++
@@ -1,6 +0,0 @@
-import time
-import webbrowser
-print "Just a note, with pushing to the master"
-time.sleep(1)
-handle = webbrowser.get()
-handle.open('http://fc02.deviantart.net/fs70/f/2012/240/2/1/profile_picture_by_beccastahpplz-d5crx6n.jpg')
