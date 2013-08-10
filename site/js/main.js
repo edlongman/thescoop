7a38f5cb3e8ba3ac0ec39ee061b36367738bc8c2
bleko <bleko@server.fake>
2013-08-10 23:47:47 +0100

    Javascript commenting, new clipping script

--- site/js/main.js
+++ site/js/main.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-	// Resize select and input
+$(document).ready(function() {
 	resizeSection();
 	resizeNumber();
 	resizeDate();
@@ -10,49 +9,55 @@ $(document).ready(function(){
 		resizeDate();
 	});
 
-	// Fetching and displaying stories
 	getNews();
 
-	// onchange of input fields
-	$('#section').change(function(){
+	$('select[name="section"]').change(function() {
+		console.log('success');
 		resizeSection();
 		getNews();
 	});
-	$('#number').bind('keyup input paste', function(){
-		resizeNumber();
-        if ($(this).val() != ''){
-            getNews();
-        }
 
-		var first_option = $('#date option:first-child').text();
+	$('input[name="number"]').bind('keyup input paste', function() {
+		resizeNumber();
+		if ($(this).val() != ''){
+			getNews();
+		}
+
+		// Change days to day if value is 1 etc.
+
+		var first_option = $('select[name="date"] option:first-child').text();
 		var last_letter = first_option.substr(first_option.length - 1);
 
 		if (last_letter == 's' && $(this).val() == 1) {
-			$('#date option').each(function() {
+			$('select[name="date"] option').each(function() {
 				var removed_s = $(this).text().slice(0, -1);
 				$(this).text($(this).text().slice(0, -1));
 			})
 		}
 		else if (last_letter != 's' && $(this).val() != 1) {
-			$('#date option').each(function() {
+			$('select[name="date"] option').each(function() {
 				old_text = $(this).text();
 				$(this).text(old_text + 's');
 			})
 		}
 
 	});
-	$('#date').change(function(){
+
+	$('select[name="date"]').change(function(){
 		resizeDate();
 		getNews();
 	});
 });
 
+
+// Get headlines
+
 function getNews(){
 	$('.news').html('<img src="img/loading.gif">');
 
-	amount = $('#number').val();
-	scope = $('#date option:selected').val();
-	section = $('#section option:selected').val();
+	amount = $('input[name="number"]').val();
+	scope = $('select[name="date"] option:selected').val();
+	section = $('select[name="section"] option:selected').val();
 	// keyword $('#keyword').val()
 	keyword = '';
 
@@ -63,11 +68,14 @@ function getNews(){
 		// make asynchronous request
 		getGuardianNews(amount, scope, section, keyword);
 	} catch (e) {
-        ajax.abort(); // using global variable containing current ajax request
+		ajax.abort(); // using global variable containing current ajax request
 		$('.news').html('<p class="error">Please assure your input is correct (' + e + ')</p>')
 		return;
 	}
 }
+
+
+// Loading headlines to page
 
 function handleGuardianNews(news){
 	str = '<ol>';
@@ -87,13 +95,16 @@ function handleGuardianNews(news){
 	});
 	str += '</ol>';
 	$('.news').slideUp(300, function(){
-        $('.news').html(str);
-        $('.news article').hide();
-        $('.news').slideDown(300, function(){
-            initializeLinkListeners();
-        });
-    });
+		$('.news').html(str);
+		$('.news article').hide();
+		$('.news').slideDown(300, function(){
+			initializeLinkListeners();
+		});
+	});
 }
+
+
+// On headline click...
 
 function initializeLinkListeners() {
 	var articles = $('.headline').next('article');
@@ -103,15 +114,8 @@ function initializeLinkListeners() {
 		var headline = $(this);
 		var article = headline.next('article');
 
-		article.slideToggle({
-			duration: 400,
-			easing: 'easeInOutCirc'
-		});
-
-		articles.not(article).slideUp({
-			duration: 400,
-			easing: 'easeInOutCirc'
-		});
+		article.slideToggle(300);
+		articles.not(article).slideUp(300);
 
 		if (! headline.hasClass('loaded')) {
 			getSummary(headline);
@@ -121,47 +125,51 @@ function initializeLinkListeners() {
 }
 
 function initializeTryAgain () {
-    $('.try-again').click(function(event) {
-        event.preventDefault();
+	$('.try-again').click(function(event) {
+		event.preventDefault();
 
-        if ($(this).hasClass('try-again--news')) {
-            getNews();
-        } else if ($(this).hasClass('try-again--summary')) {
-            getSummary($(this).parent().parent().parent().prev('h2'));
-        }
-    });
+		if ($(this).hasClass('try-again--news')) {
+			getNews();
+		} else if ($(this).hasClass('try-again--summary')) {
+			getSummary($(this).parent().parent().parent().prev('h2'));
+		}
+	});
 }
 
+
+// Resize functions
+
 function resizeSection() {
-	option_val = $('#section > option:selected').text();
-	$('#section-span').html(option_val);
-	$('#section').width($('#section-span').width() + 3);
+	option_val = $('select[name="section"] > option:selected').text();
+	$('.form-span').html(option_val);
+	$('select[name="section"]').width($('.form-span').width());
 }
 
 function resizeNumber() {
-	$('#number-span').html($('#number').val());
-	$('#number').css('width', $('#number-span').width() + 3);
+	$('.form-span').html($('input[name="number"]').val());
+	$('input[name="number"]').css('width', $('.form-span').width() + 2);
 
 	var font_size = $('body').css('font-size');
 
 	if (font_size == '16px') {
-		$('#number').css('height', '42px');
+		$('input[name="number"]').css('height', '42px');
 	}
-
 	else if (font_size == '14px') {
-		$('#number').css('height', '36px');
+		$('input[name="number"]').css('height', '37px');
 	}
-
 	else if (font_size == '12px') {
-		$('#number').css('height', '30px');
+		$('input[name="number"]').css('height', '31px');
 	}
 }
 
 function resizeDate() {
-	option_val = $('#date > option:selected').val();
-	$('#date-span').html(option_val);
-	$('#date').width($('#date-span').width() + 3);
+	option_val = $('select[name="date"] > option:selected').val();
+	$('.form-span').html(option_val);
+	$('select[name="date"]').width($('.form-span').width() + 2);
 }
+
+
+// Form validation
 
 function validate(amount, scope, section, keyword) {
 	// scopes = ['days', 'weeks', 'months'];
@@ -170,7 +178,7 @@ function validate(amount, scope, section, keyword) {
 	// }
 
 	if (isNaN(amount)) {
-		throw 'Invalid amount of ' + $('#date option:selected').val();
+		throw 'Invalid amount of ' + $('select[name="date"] option:selected').val();
 	}
 
 	// sections = ['world', 'uk-news', 'football', 'film', 'business', 'politics', 'technology'];
@@ -184,69 +192,74 @@ function validate(amount, scope, section, keyword) {
 ajax = null;
 
 function getGuardianNews(amount, scope, section, keyword){
-    today = new Date(); // dates are entered relatively, today is needed
+	today = new Date(); // dates are entered relatively, today is needed
 
-    start_time = new Date();
-    end_time = today;
-    //section = section;
+	start_time = new Date();
+	end_time = today;
+	//section = section;
 
-    // get correct start time
-    switch (scope) {
-        case 'days': start_time.setDate(today.getDate()-amount); break;
-        case 'weeks': start_time.setDate(today.getDate()-amount*7); break;
-        case 'months': start_time.setMonth(today.getMonth()-amount); break;
-    }
+	// get correct start time
+	switch (scope) {
+		case 'days': start_time.setDate(today.getDate()-amount); break;
+		case 'weeks': start_time.setDate(today.getDate()-amount*7); break;
+		case 'months': start_time.setMonth(today.getMonth()-amount); break;
+	}
 
-    // make asynchronous ajax request, calls handle
-    ajaxGuardian(start_time, end_time, section, keyword);
+	// make asynchronous ajax request, calls handle
+	ajaxGuardian(start_time, end_time, section, keyword);
 }
 
 function ajaxGuardian(start_time, end_time, section, keyword){
-    ajax = $.ajax({
-        url: 'guardian_feeds.php',
-        type: 'GET',
-        dataType: 'json',
-        data: {start_time: start_time.f('yyyy-MM-dd'), end_time: end_time.f('yyyy-MM-dd'), section: section, keyword: keyword},
-        success: function(data, textStatus, xhr) {
-            // replace JSON date format with JavaScript Date Objects
-            $.each(data, function(index, story) {
-                story[2] = new Date(story[2]);
-            });
+	ajax = $.ajax({
+		url: 'guardian_feeds.php',
+		type: 'GET',
+		dataType: 'json',
+		data: {start_time: start_time.f('yyyy-MM-dd'), end_time: end_time.f('yyyy-MM-dd'), section: section, keyword: keyword},
+		success: function(data, textStatus, xhr) {
+			// replace JSON date format with JavaScript Date Objects
+			$.each(data, function(index, story) {
+				story[2] = new Date(story[2]);
+			});
 
-            // call handleGuardianNews function of main.js
-            handleGuardianNews(data);
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            $('.news').html('<p class="error">Couldn’t scoop the news for you&hellip; <a href="#" title="Try again" class="try-again try-again--news">Try again</a></p>');
-            initializeTryAgain();
-            console.log('ERROR: ' + errorThrown);
-        }
-    });
+			// call handleGuardianNews function of main.js
+			handleGuardianNews(data);
+		},
+		error: function(xhr, textStatus, errorThrown) {
+			$('.news').html('<p class="error">Couldn’t scoop the news for you&hellip; <a href="#" title="Try again" class="try-again try-again--news">Try again</a></p>');
+			initializeTryAgain();
+			console.log('ERROR: ' + errorThrown);
+		}
+	});
 }
 
 function getSummary (object) {
-    ajax = $.ajax({
-        url: 'ots.php',
-        type: 'GET',
-        dataType: 'html',
-        data: {to_sum: $(object).next('article').find('a.read-more').attr('href'), ratio: 10},
-        success: function(data, textStatus, xhr) {
-            data = $('<div/>').html(data).text();
-            if (data == 'null') {
-                data = 'No summary found.'
-            }
-            $(object).next('article').find('.summary--content').slideUp(200, function(){
-                $(object).next('article').find('.summary--content').html('<p>' + data + '</p>');
-                $(object).next('article').find('.summary--content').find('a').attr('target', '_blank');
-                object.addClass('loaded');
-            }).slideDown(300);
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            $(object).next('article').find('.summary--content').html('<p class="error">Couldn’t get summary&hellip;<a href="#" title="Try again" class="try-again try-again--summary">Try again</a></p>');
-            initializeTryAgain();
-            console.log('ERROR in getSummary');
-            console.log(xhr);
-        }
-    });
-    
+	ajax = $.ajax({
+		url: 'ots.php',
+		type: 'GET',
+		dataType: 'html',
+		data: {to_sum: $(object).next('article').find('a.read-more').attr('href'), ratio: 5},
+		success: function(data, textStatus, xhr) {
+			data = $('<div/>').html(data).text();
+			if (data == 'null') {
+				data = 'No summary found.'
+			}
+			$(object).next('article').find('.summary--content').slideUp(300, function(){
+				$(object).next('article').find('.summary--content').html('<p>' + data + '</p>');
+				$(object).next('article').find('.summary--content').find('a').attr('target', '_blank');
+				object.addClass('loaded');
+			}).slideDown(400);
+			// $(object).next('article').find('.summary--content').slideUp(300, function(){
+			//     $(object).next('article').find('.summary--content').html('<p>' + data + '</p>');
+			//     $(object).next('article').find('.summary--content').find('a').attr('target', '_blank');
+			//     object.addClass('loaded');
+			// }).slideDown(400);
+		},
+		error: function(xhr, textStatus, errorThrown) {
+			$(object).next('article').find('.summary--content').html('<p class="error">Couldn’t get summary&hellip;<a href="#" title="Try again" class="try-again try-again--summary">Try again</a></p>');
+			initializeTryAgain();
+			console.log('ERROR in getSummary');
+			console.log(xhr);
+		}
+	});
+	
 }
