f629f15d8c182ade666e1ae425687f869aa2add8
Taiiwo Llort <taiiwo@localhost.localdomain>
2013-08-12 18:53:11 +0100

    ots will now return null on guardian videos. This is a quick and dirty fix. I'm so sorry

--- site/ots.php
+++ site/ots.php
@@ -28,8 +28,12 @@
 			$sum = shell_exec ( 'ots -r ' . $ratio . ' -d en txt.txt');
 			fwrite( $file , '' );
 			fclose( $file );
-			//$sum = str_replace('.','. ',$sum);
-			$sum = htmlspecialchars($sum);
+			if (strpos($sum,'When autoplay is on, videos on these pages will autoplay')) {
+				$sum = 'null';
+			}
+			else {
+				$sum = htmlspecialchars($sum);
+			}
 		}
 		else {
 			$sum = 'null';
