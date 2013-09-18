35c7f0b235efc4dab9ae1802dad53de59f8969cd
Edward Longman <ed@edlog.co.uk>
2013-09-18 21:04:39 +0100

    split into functions and change loops

--- update_categories.py
+++ update_categories.py
@@ -5,12 +5,21 @@ sql = MySQLdb.connect(host="localhost",
                             db=config.db)
 sql.query("SELECT `site` FROM  `sites` ")
 sites=sql.store_result().fetch_row(0)
-print "{"
-for site in sites:
+
+def print_site_urls(site):
     print "'"+site[0]+"':{"
     sql.query("SELECT  `sites`.`site` ,  `url`, `section` FROM  `feedurls` ,  `sites` WHERE  `sites`.`site` =  '"+site[0]+"'")
     feeds=sql.store_result().fetch_row(0)
     for feed in feeds:
-        print "'"+feed[2]+"':'"+feed[1]+"',"
+        print_feed_data()
     print "}"
+
+def print_feed_data():
+    print "'"+feed[2]+"':'"+feed[1]+"',"
+print "{"
+i=1
+print_site_data(sites[0])
+while i<len(sites):
+    print_site_data(sites[i])
+    i+=1
 print "}"
