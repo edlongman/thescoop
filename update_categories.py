e4c26bc8a59c3df118cb4aa0272cc8ca6984574b
Edward Longman <ed@edlog.co.uk>
2013-09-18 20:57:02 +0100

    get sites from database

--- update_categories.py
+++ update_categories.py
@@ -3,3 +3,11 @@ sql = MySQLdb.connect(host="localhost",
                             user=config.username, 
                             passwd=config.passwd,
                             db=config.db)
+sql.query("SELECT `site` FROM  `sites` ")
+sites=sql.store_result().fetch_row(0)
+print "{"
+for site in sites:
+    print "'"+site[0]+"':{"
+    sql.query("SELECT  `sites`.`site` ,  `url` FROM  `feedurls` ,  `sites` WHERE  `sites`.`site` =  '"+site[0]+"'")
+    sites=sql.store_result().fetch_row(0)
+    print "}"
