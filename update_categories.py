3a3e6b5b6f9b027bfeacd366fa6e955b242f80e9
Edward Longman <ed@edlog.co.uk>
2013-10-01 21:48:50 +0100

    created category scraper

--- update_categories.py
+++ update_categories.py
@@ -5,26 +5,33 @@ sql = MySQLdb.connect(host="localhost",
                             db=config.db)
 sql.query("SELECT `site` FROM  `sites` ")
 sites=sql.store_result().fetch_row(0)
-
+output=""
 def print_site_data(site):
-    print '"'+site[0]+'":{'
+    output=""
+    output+= '"'+site[0]+'":{'
     sql.query("SELECT  `sites`.`site` ,  `url`, `section` FROM  `feedurls` ,  `sites` WHERE  `sites`.`site` =  '"+site[0]+"'")
     feeds=sql.store_result().fetch_row(0)
     i=1
     print_feed_data(feeds[0])
     while i<len(feeds):
-        print ","
-        print_feed_data(feeds[i])
+        output+= ","
+        output+=print_feed_data(feeds[i])
         i+=1
-    print "}"
+    output+= "}"
+    return output
 
 def print_feed_data(feed):
-    print '"'+feed[2]+'":"'+feed[1]+'"'
-print "{"
+    output=""
+    output+= '"'+feed[2]+'":"'+feed[1]+'"'
+    return output
+output+= "{"
 i=1
-print_site_data(sites[0])
+output+=print_site_data(sites[0])
 while i<len(sites):
-    print ","
-    print_site_data(sites[i])
+    output+= ","
+    output+=print_site_data(sites[i])
     i+=1
-print "}"
+output+=',"guardian":{"business":"business","education":"education","environment":"environment","film":"film","football":"football","media":"media","politics":"politics","science":"science","sport":"sport","technology":"technology","uk-news":"uk-news","world":"world"}'
+output+= "}"
+file=open('site/sites.json',"w")
+file.write(output)
