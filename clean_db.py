2388d8f0c29a50e4569f716c66057a915986b44d
Edward Longman <eialongman@gmail.com>
2013-11-22 22:25:20 +0000

    fix variable name

--- clean_db.py
+++ clean_db.py
@@ -4,14 +4,14 @@ sql = MySQLdb.connect(host="localhost",
                             user=config.username, 
                             passwd=config.passwd,
                             db=config.test_db)
-sql.query("SELECT `site`,`section`,`url`,`id` FROM `feedurls`")
+sql.query("SELECT `id` FROM `feedurls`")
 db_feed_query=sql.store_result()
 rss_urls=db_feed_query.fetch_row(0)
 table_name = "stories"
 date_from = datetime.strptime(raw_input("start date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
 date_to = datetime.strptime(raw_input("end date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
-for rss_urls_data in rss_urls:
-    feed_id=rss_url_data[3]
+for rss_url_data in rss_urls:
+    feed_id=rss_url_data[0]
     i = start_date 
     while i <= end_date:
         print end_dates
