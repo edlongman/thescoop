051d307fcd33441a310c98043dab19cc7b556a66
Edward Longman <eialongman@gmail.com>
2013-11-22 22:22:19 +0000

    change to raw input

--- clean_db.py
+++ clean_db.py
@@ -8,8 +8,8 @@ sql.query("SELECT `site`,`section`,`url`,`id` FROM `feedurls`")
 db_feed_query=sql.store_result()
 rss_urls=db_feed_query.fetch_row(0)
 table_name = "stories"
-date_from = datetime.strptime(input("start date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
-date_to = datetime.strptime(input("end date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
+date_from = datetime.strptime(raw_input("start date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
+date_to = datetime.strptime(raw_input("end date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
 for rss_urls_data in rss_urls:
     feed_id=rss_url_data[3]
     i = start_date 
