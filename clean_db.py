3e6259ed123de8cfc09bd090b4080fff93c41208
Edward Longman <eialongman@gmail.com>
2013-11-22 23:29:24 +0000

    modify query to fix where clause error and date error

--- clean_db.py
+++ clean_db.py
@@ -14,10 +14,10 @@ for rss_url_data in rss_urls:
     i = date_from 
     while i <= date_to:
         print i.strftime("%d/%m/%Y")
-        whereclause="`date_added` = '" + i.strftime("%d/%m/%Y") + "'"
+        whereclause="`date_added` = '" + i.strftime("%d-%m-%Y") + "'"
         whereclause+=" AND `feedid`= "+ str(feed_id) +"'"
         query="DELETE stories WHERE " + whereclause 
-        query+="AND `url` NOT IN (SELECT `url`,`points` FROM stories "+whereclause+" ORDER BY `points` DESC LIMIT 0,20);"
+        query+="AND `url` NOT IN (SELECT `url`,`points` FROM stories WHERE "+whereclause+" ORDER BY `points` DESC LIMIT 0,20);"
         print query;
         sql.query(query)
         sql.commit()
