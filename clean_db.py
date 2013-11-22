6a9aca1125c9e82a0486fa14bc5e9154a47672c9
Edward Longman <eialongman@gmail.com>
2013-11-22 22:35:32 +0000

    change too long datetime write out

--- clean_db.py
+++ clean_db.py
@@ -1,5 +1,4 @@
 import MySQLdb, config, urllib, cgi, datetime
-from datetime import datetime
 sql = MySQLdb.connect(host="localhost", 
                             user=config.username, 
                             passwd=config.passwd,
@@ -8,12 +7,12 @@ sql.query("SELECT `id` FROM `feedurls`")
 db_feed_query=sql.store_result()
 rss_urls=db_feed_query.fetch_row(0)
 table_name = "stories"
-date_from = datetime.strptime(raw_input("start date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
-date_to = datetime.strptime(raw_input("end date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
+date_from = datetime.datetime.strptime(raw_input("start date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
+date_to = datetime.datetime.strptime(raw_input("end date inc. in form 'dd-mm-yyyy'"),"%d-%m-%Y")
 for rss_url_data in rss_urls:
     feed_id=rss_url_data[0]
     i = date_from 
     while i <= date_to:
         print i.strftime("%d/%m/%Y")
-        i += timedelta(days=1)
+        i += datetime.timedelta(days=1)
  
