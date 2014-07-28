ce56df8f9ef000228ee2f013d8cd3f13f607e387
edlongman <edlongman@users.noreply.github.com>
2014-07-28 21:41:36 +0100

    added timing function to cleanup function to see server improvement

--- clean_db.py
+++ clean_db.py
@@ -1,4 +1,4 @@
-import MySQLdb, config, urllib, cgi, datetime
+import MySQLdb, config, urllib, cgi, datetime, time
 sql = MySQLdb.connect(host="localhost", 
                             user=config.username, 
                             passwd=config.passwd,
@@ -13,13 +13,14 @@ for rss_url_data in rss_urls:
     feed_id=rss_url_data[0]
     i = date_from 
     while i <= date_to:
+        t0=time.clock()
         print i.strftime("%d/%m/%Y")
         whereclause="`date_added` = '" + i.strftime("%Y-%m-%d") + "'"
         whereclause+=" AND `feedid`= "+ str(feed_id) +""
         query="DELETE FROM stories WHERE " + whereclause 
         query+=" AND `url` NOT IN (SELECT * FROM (SELECT `url` FROM stories WHERE "+whereclause
         query+=" ORDER BY `points` DESC LIMIT 0,20) AS TAB);"
-        print query;
+        print(time.clock()-t0)
         sql.query(query)
         sql.commit()
         i += datetime.timedelta(days=1)
